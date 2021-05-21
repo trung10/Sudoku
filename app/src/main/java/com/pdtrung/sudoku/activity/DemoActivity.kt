1a7940cefc89bee340f546c78518c4625e057dab
DESKTOP-LEAP17G\B1nTen <phamductrung10@gmail.com>
2021-05-21 17:45:22 +0700

    Update

---
+++ app/src/main/java/com/pdtrung/sudoku/activity/DemoActivity.kt
@@ -0,0 +1,12 @@
+package com.pdtrung.sudoku.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pdtrung.sudoku.R
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
